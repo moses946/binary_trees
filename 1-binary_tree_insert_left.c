8c2e60dda6e4d2c749cc5a3d10f9c08cf1783ac0
moses946 <mosesmwaiw@gmail.com>
2024-03-03 11:54:35 +0300

    Update existing left node's parent

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,10 +13,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 
 	if (!parent)
-		return NULL;
+		return (NULL);
 	new_node = binary_tree_node(parent, value);
 	if (parent->left)
 		new_node->left = parent->left;
+		new_node->left->parent = new_node;
 	parent->left = new_node;
-	return new_node;
+
+	return (new_node);
 }
