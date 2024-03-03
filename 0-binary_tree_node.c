fcc12a843420daff904d8bc8c282e58171169612
moses946 <mosesmwaiw@gmail.com>
2024-03-03 11:48:30 +0300

    Fix memory leaks

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,7 +2,6 @@
 
 /**
   * binary_tree_node - Creates a binary tree node
-  *
   * @parent: Parent node to the new node
   * @value: Value of the node
   *
@@ -13,16 +12,12 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 
 	new_node = malloc(sizeof(binary_tree_t));
-	new_node->left = malloc(sizeof(binary_tree_t));
-	new_node->right = malloc(sizeof(binary_tree_t));
-	new_node->parent = malloc(sizeof(binary_tree_t));
-	if (!new_node || !new_node->left)
-		return NULL;
-	if (!new_node->parent || !new_node->right)
-		return NULL;
+	if (!new_node)
+		return (NULL);
+
 	new_node->parent = parent;
 	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
-	return new_node;
+	return (new_node);
 }
