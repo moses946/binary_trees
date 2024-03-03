b047206b56e5e2430e01abb0d376b4a2940b88ac
moses946 <mosesmwaiw@gmail.com>
2024-03-03 11:57:34 +0300

    Update exisiting right child node

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,10 +13,13 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 
 	if (!parent)
-		return NULL;
+		return (NULL);
 	new_node = binary_tree_node(parent, value);
 	if (parent->right)
+	{
 		new_node->right = parent->right;
+		new_node->right->parent = new_node;
+	}
 	parent->right = new_node;
-	return new_node;
+	return (new_node);
 }
