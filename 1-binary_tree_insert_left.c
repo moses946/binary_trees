0cbf2af4a7ea09a76ca58fd23a03053b6b7bfd37
moses946 <mosesmwaiw@gmail.com>
2024-03-03 11:56:12 +0300

    Add curly braces around if statement

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,8 +16,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	new_node = binary_tree_node(parent, value);
 	if (parent->left)
+	{
 		new_node->left = parent->left;
 		new_node->left->parent = new_node;
+	}
 	parent->left = new_node;
 
 	return (new_node);
